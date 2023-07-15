b58299ef805d995daeddaf686a66b449691aca7e
小傅哥 <184172133@qq.com>
2023-07-16 07:57:44 +0800

    feat: init chatgpt data project by ddd

---
+++ chatgpt-data-app/src/test/java/cn/bugstack/chatgpt/data/test/ApiTest.java
@@ -0,0 +1,20 @@
+package cn.bugstack.chatgpt.data.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 测试工程
+ * @create 2023-07-16 07:49
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+
+
+
+}
