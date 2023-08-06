6c13f915ae4018db4050297bc22f95941fc4a17b
小傅哥 <184172133@qq.com>
2023-08-06 11:56:59 +0800

    feat: 第5节：公众号发送验证码鉴权登录

--- chatgpt-data-domain/src/main/java/cn/bugstack/chatgpt/data/domain/openai/service/AbstractChatService.java
+++ chatgpt-data-domain/src/main/java/cn/bugstack/chatgpt/data/domain/openai/service/AbstractChatService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
@@ -22,28 +23,21 @@ public abstract class AbstractChatService implements IChatService {
     protected OpenAiSession openAiSession;
 
     @Override
-    public ResponseBodyEmitter completions(ChatProcessAggregate chatProcess) {
-        // 1. 校验权限
-        if (!"b8b6".equals(chatProcess.getToken())) {
-            throw new ChatGPTException(Constants.ResponseCode.TOKEN_ERROR.getCode(), Constants.ResponseCode.TOKEN_ERROR.getInfo());
-        }
-
-        // 2. 请求应答
-        ResponseBodyEmitter emitter = new ResponseBodyEmitter(3 * 60 * 1000L);
+    public ResponseBodyEmitter completions(ResponseBodyEmitter emitter, ChatProcessAggregate chatProcess) {
+        // 1. 请求应答
         emitter.onCompletion(() -> {
             log.info("流式问答请求完成，使用模型：{}", chatProcess.getModel());
         });
-
         emitter.onError(throwable -> log.error("流式问答请求疫情，使用模型：{}", chatProcess.getModel(), throwable));
 
-        // 3. 应答处理
+        // 2. 应答处理
         try {
             this.doMessageResponse(chatProcess, emitter);
         } catch (Exception e) {
             throw new ChatGPTException(Constants.ResponseCode.UN_ERROR.getCode(), Constants.ResponseCode.UN_ERROR.getInfo());
         }
 
-        // 4. 返回结果
+        // 3. 返回结果
         return emitter;
     }
 
