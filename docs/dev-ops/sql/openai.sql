8cec5d4ef6d46d0f3c8f807b47e654a8041b30d9
小傅哥 <184172133@qq.com>
2023-10-05 13:10:27 +0800

    feat: sql 语句

--- docs/dev-ops/sql/openai.sql
+++ docs/dev-ops/sql/openai.sql
@@ -7,7 +7,7 @@
 #
 # 主机: 127.0.0.1 (MySQL 5.6.39)
 # 数据库: openai
-# 生成时间: 2023-10-04 02:52:50 +0000
+# 生成时间: 2023-10-05 02:16:49 +0000
 # ************************************************************
 
 
@@ -21,6 +21,54 @@ SET NAMES utf8mb4;
 
 CREATE database if NOT EXISTS `openai` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
 use `openai`;
+
+# 转储表 openai_order
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `openai_order`;
+
+CREATE TABLE `openai_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `openid` varchar(128) NOT NULL COMMENT '用户ID；微信分配的唯一ID编码',
+  `product_id` int(4) NOT NULL COMMENT '商品ID',
+  `product_name` varchar(32) NOT NULL COMMENT '商品名称',
+  `product_quota` int(8) NOT NULL COMMENT '商品额度',
+  `order_id` varchar(12) NOT NULL COMMENT '订单编号',
+  `order_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '下单时间',
+  `order_status` tinyint(1) NOT NULL COMMENT '订单状态；0-创建完成、1-等待发货、2-发货完成、3-系统关单',
+  `total_amount` decimal(10,2) NOT NULL COMMENT '订单金额',
+  `pay_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '支付方式；0-微信支付',
+  `pay_url` varchar(128) DEFAULT NULL COMMENT '支付地址；创建支付后，获得的URL地址',
+  `pay_amount` decimal(10,2) DEFAULT NULL COMMENT '支付金额；支付成功后，以回调信息更新金额',
+  `transaction_id` varchar(32) DEFAULT NULL COMMENT '交易单号；支付成功后，回调信息的交易单号',
+  `pay_status` tinyint(1) DEFAULT NULL COMMENT '支付状态；0-等待支付、1-支付完成、2-支付失败、3-放弃支付',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# 转储表 openai_product
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `openai_product`;
+
+CREATE TABLE `openai_product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `product_id` int(4) NOT NULL COMMENT '商品ID',
+  `product_name` varchar(32) NOT NULL COMMENT '商品名称',
+  `product_desc` varchar(128) NOT NULL COMMENT '商品描述',
+  `quota` int(8) NOT NULL COMMENT '额度次数',
+  `price` decimal(10,2) NOT NULL COMMENT '商品价格',
+  `sort` int(4) NOT NULL COMMENT '商品排序',
+  `is_enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否有效；0无效、1有效',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 
 # 转储表 user_account
 # ------------------------------------------------------------
@@ -46,7 +94,7 @@ LOCK TABLES `user_account` WRITE;
 
 INSERT INTO `user_account` (`id`, `openid`, `total_quota`, `surplus_quota`, `model_types`, `status`, `create_time`, `update_time`)
 VALUES
-	(1,'xfg',10,0,'gpt-3.5-turbo,gpt-3.5-turbo-16k',0,'2023-10-03 18:56:13','2023-10-04 10:50:23');
+	(1,'xfg',10,0,'gpt-3.5-turbo,gpt-3.5-turbo-16k',0,'2023-10-03 18:56:13','2023-10-04 14:36:09');
 
 /*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
 UNLOCK TABLES;
