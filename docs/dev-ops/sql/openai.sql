28d883137b24d181f3a35a535e78d1e27be1ddba
小傅哥 <184172133@qq.com>
2023-10-06 16:56:34 +0800

    docs: 工程所需库表初始化SQL

--- docs/dev-ops/sql/openai.sql
+++ docs/dev-ops/sql/openai.sql
@@ -7,7 +7,7 @@
 #
 # 主机: 127.0.0.1 (MySQL 5.6.39)
 # 数据库: openai
-# 生成时间: 2023-10-05 02:16:49 +0000
+# 生成时间: 2023-10-05 23:19:49 +0000
 # ************************************************************
 
 
@@ -33,6 +33,7 @@ CREATE TABLE `openai_order` (
   `product_id` int(4) NOT NULL COMMENT '商品ID',
   `product_name` varchar(32) NOT NULL COMMENT '商品名称',
   `product_quota` int(8) NOT NULL COMMENT '商品额度',
+  `product_model_types` varchar(128) DEFAULT NULL COMMENT '可用模型；gpt-3.5-turbo,gpt-3.5-turbo-16k,gpt-4,gpt-4-32k',
   `order_id` varchar(12) NOT NULL COMMENT '订单编号',
   `order_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '下单时间',
   `order_status` tinyint(1) NOT NULL COMMENT '订单状态；0-创建完成、1-等待发货、2-发货完成、3-系统关单',
@@ -42,11 +43,23 @@ CREATE TABLE `openai_order` (
   `pay_amount` decimal(10,2) DEFAULT NULL COMMENT '支付金额；支付成功后，以回调信息更新金额',
   `transaction_id` varchar(32) DEFAULT NULL COMMENT '交易单号；支付成功后，回调信息的交易单号',
   `pay_status` tinyint(1) DEFAULT NULL COMMENT '支付状态；0-等待支付、1-支付完成、2-支付失败、3-放弃支付',
+  `pay_time` datetime DEFAULT NULL COMMENT '支付时间',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+LOCK TABLES `openai_order` WRITE;
+/*!40000 ALTER TABLE `openai_order` DISABLE KEYS */;
+
+INSERT INTO `openai_order` (`id`, `openid`, `product_id`, `product_name`, `product_quota`, `product_model_types`, `order_id`, `order_time`, `order_status`, `total_amount`, `pay_type`, `pay_url`, `pay_amount`, `transaction_id`, `pay_status`, `pay_time`, `create_time`, `update_time`)
+VALUES
+	(1,'xfg',1001,'测试商品',100,NULL,'118845061424','2023-10-05 17:02:42',0,0.11,0,'weixin://wxpay/bizpayurl?pr=NFByxsizz',NULL,NULL,1,NULL,'2023-10-05 17:02:41','2023-10-05 20:08:29'),
+	(2,'xfg',1001,'测试商品',100,NULL,'730807176035','2023-10-05 20:35:46',2,0.11,0,'weixin://wxpay/bizpayurl?pr=0ckFdM2zz',0.11,'4200001993202310054200967494',1,'2023-10-05 20:09:10','2023-10-05 20:08:46','2023-10-05 20:42:16'),
+	(3,'xfg',1001,'测试商品',100,NULL,'957726718857','2023-10-05 21:10:37',0,0.11,0,'weixin://wxpay/bizpayurl?pr=5BWAAAezz',NULL,NULL,0,NULL,'2023-10-05 21:10:37','2023-10-05 21:10:37');
+
+/*!40000 ALTER TABLE `openai_order` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # 转储表 openai_product
@@ -59,15 +72,26 @@ CREATE TABLE `openai_product` (
   `product_id` int(4) NOT NULL COMMENT '商品ID',
   `product_name` varchar(32) NOT NULL COMMENT '商品名称',
   `product_desc` varchar(128) NOT NULL COMMENT '商品描述',
+  `product_model_types` varchar(128) DEFAULT NULL COMMENT '可用模型；gpt-3.5-turbo,gpt-3.5-turbo-16k,gpt-4,gpt-4-32k',
   `quota` int(8) NOT NULL COMMENT '额度次数',
   `price` decimal(10,2) NOT NULL COMMENT '商品价格',
   `sort` int(4) NOT NULL COMMENT '商品排序',
   `is_enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否有效；0无效、1有效',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idx_product_id_enabled` (`product_id`,`is_enabled`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+LOCK TABLES `openai_product` WRITE;
+/*!40000 ALTER TABLE `openai_product` DISABLE KEYS */;
+
+INSERT INTO `openai_product` (`id`, `product_id`, `product_name`, `product_desc`, `product_model_types`, `quota`, `price`, `sort`, `is_enabled`, `create_time`, `update_time`)
+VALUES
+	(1,1001,'测试商品','测试商品请勿下单',NULL,100,0.11,1,1,'2023-10-05 16:51:47','2023-10-05 16:51:47');
+
+/*!40000 ALTER TABLE `openai_product` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # 转储表 user_account
@@ -94,7 +118,7 @@ LOCK TABLES `user_account` WRITE;
 
 INSERT INTO `user_account` (`id`, `openid`, `total_quota`, `surplus_quota`, `model_types`, `status`, `create_time`, `update_time`)
 VALUES
-	(1,'xfg',10,0,'gpt-3.5-turbo,gpt-3.5-turbo-16k',0,'2023-10-03 18:56:13','2023-10-04 14:36:09');
+	(1,'xfg',210,200,'gpt-3.5-turbo,gpt-3.5-turbo-16k',0,'2023-10-03 18:56:13','2023-10-05 20:42:16');
 
 /*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
 UNLOCK TABLES;
